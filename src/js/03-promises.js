8a19f5e45e2e8df510fe99360cc67692b152c76d
TetianaUMKA <113261486+TetianaUMKA@users.noreply.github.com>
2023-03-11 20:47:11 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delay = document.querySelector('input[name="delay"]');
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const btnCreatePromise = document.querySelector('button[type="submit"]'); 
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +14,4 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
